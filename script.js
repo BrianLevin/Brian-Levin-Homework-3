46bdf2a6948133d4d7468445aaef82e1a0a38bc6
Brian Levin <bml201095@gmail.com>
2020-03-12 00:08:33 -0400

    Created a function which contains if statements of the differant character choices a user can make to help generate the password

--- script.js
+++ script.js
@@ -54,3 +54,25 @@ generateBtn.addEventListener("click", function () {
     var charTypeNumber = prompt("do you want numbers in your password?");
     var charTypeSpecial = prompt("do you want special characters in your password");
 });
+// created a function which will help set up and generate the password
+function generatePassword()
+// created the charSet varible to hold the users potential choices of characters that  the javascript will utilize
+var charSet = " ";
+var charTypeLower2 = charType.toLowerCase();
+//Made  if and else if statements for each case the user may choose
+if (charTypeLower === "lower case") {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+}
+
+else if (charTypeUpper === "upper case") {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+else if (charTypeNumber === "number") {
+    charSet = "0123456789";
+}
+else if (charTypeSpecial === "special") {
+    charSet = "!@#$%^&*()_+";
+}
+
+
