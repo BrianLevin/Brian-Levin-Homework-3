043204892838b03aa32d33e8fe9f3c5aa0de9a38
Brian Levin <bml201095@gmail.com>
2020-03-11 22:58:44 -0400

    Found a working javascript button online and put it in my js file

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+
+//var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+
+//ask for character type
+//var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+//generate password
+//function generatePassword() {
+//evaluate character type
+//var charSet = "";
+//var charTypeLower = charType.toLowerCase();
+//if (charTypeLower === "lowercase") {
+//charSet = "abcdefghijklmnopqrstuvwxyz";
+//} else if (charTypeLower === "uppercase") {
+//charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//} else if (charTypeLower === "numeric") {
+// charSet = "0123456789";
+//} else if (charTypeLower === "special") {
+//charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//}
+//return value
+//var retVal = "";
+//for (var i = 0; i < length; i++) {
+//picks a character within charSet at index of random number
+//retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+//}
+//return retVal;
+//}
+//alert(generatePassword());
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -11,4 +42,10 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+// Found how to make a  functioning button online and  used it as my own function.
+
+generateBtn.addEventListener("click", function () {
+    var char = alert("How many Characters do you want your passward to contain?");
+});
