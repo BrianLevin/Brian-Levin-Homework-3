579aa830252b21368572550310190363cdd701c7
Brian Levin <bml201095@gmail.com>
2020-03-14 23:46:56 -0400

    I tried making my final code work. I made additional if else statements for each variable. unforetunately they did not come up with the user input number but instead listed individual characters in order. lower case, numbers, and special characters come up when you select them individually. all the characters come up when you select all

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // created arrays to hold values
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]; //MAth.random() b
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];  //MAth.random() b
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ['!', '@', "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",];
@@ -28,15 +28,27 @@ generateBtn.addEventListener("click", function () {
     // added additional variables for my values
     var charTypeLower = confirm("Do you want lower case letters in your password?")
     var charTypeUpper = confirm(" Do you want upper case letters in your password?");
-    var charTypeNumber = confirm("do you want numbers in your password?type numbers?");
+    var charTypeNumber = confirm("do you want numbers in your password?");
     var charTypeSpecial = confirm("do you want special characters in your password?");
 
     // created a function which will help set up and generate the password
     function generatePassword() { //
 
         // created the charSet variable to help comcatnate the arrays. It will then hold the  values after each if else statement is run.
+        var charSet = []
+        //var charSet = charTypeLower.concat(charTypeUpper, charTypeNumber, charTypeSpecial); //
 
-        var charSet = charTypeLower.concat(charTypeUpper, charTypeNumber, charTypeSpecial);
+        //if (charTypeLower) [charSet = charSet.concat(TypeLowerarr)]
+        //else if (charTypeUpper) [charSet = charSet.concat(TypeUpperarr)]
+        //else if (charTypeNumber) [charSet = charSet.concat(TypeNumberarr)]
+        //else if (charTypeSpecial) [charSet = charSet.concat(TypeSpecialarr)]
+
+
+
+
+
+
+
 
         //Made  if and else if statements for each case the user may choose.
 
@@ -51,32 +63,65 @@ generateBtn.addEventListener("click", function () {
         }
 
         else if (charTypeLower === true && charTypeUpper === true && charTypeNumber === false && charTypeSpecial === false) {
-            charSet = [numbers, specialChar];
+            charSet = [lowercase, uppercase];
         }
         else if (charTypeLower === true && charTypeUpper === false && charTypeNumber === false && charTypeSpecial === false) {
             charSet = [specialChar];
+
         }
-
-
-
+        else if (charTypeUpper === true && charTypeLower === true && charTypeNumber === true && charTypeSpecial === true) {
+            charSet = [uppercase, lowercase, numbers, specialChar];
+        }
+        else if (charTypeUpper === true && charTypeLower === true && charTypeNumber === true && charTypeSpecial === false) {
+            charSet = [uppercase, lowercase, numbers,];
+        }
+        else if (charTypeUpper === true && charTypeLower === true && charTypeNumber === false && charTypeSpecial === false) {
+            charSet = [uppercase, lowercase];
+        }
+        else if (charTypeUpper === true && charTypeLower === false && charTypeNumber === false && charTypeSpecial === false) {
+            charSet = [uppercase];
+        }
+        else if (charTypeNumber === true && charTypeLower === true && charTypeUpper === true && charTypeSpecial === true) {
+            charSet = [numbers, lowercase, uppercase, specialChar];
+        }
+        else if (charTypeNumber === true && charTypeLower === true && charTypeUpper === true && charTypeSpecial === false) {
+            charSet = [numbers, lowercase, uppercase,];
+        }
+        else if (charTypeNumber === true && charTypeLower === true && charTypeUpper === false && charTypeSpecial === false) {
+            charSet = [numbers, lowercase];
+        }
+        else if (charTypeNumber === true && charTypeLower === false && charTypeUpper === false && charTypeSpecial === false) {
+            charSet = [numbers];
+        }
+        else if (charTypeSpecial === true && charTypeLower === true && charTypeUpper === true && charTypeNumber === true) {
+            charSet = [specialChar, lowercase, uppercase, numbers,];
+        }
+        else if (charTypeSpecial === true && charTypeLower === true && charTypeUpper === true && charTypeNumber === false) {
+            charSet = [specialChar, lowercase, uppercase,];
+        }
+        else if (charTypeSpecial === true && charTypeLower === true && charTypeUpper === false && charTypeNumber === false) {
+            charSet = [specialChar, lowercase,];
+        }
+        else if (charTypeSpecial === true && charTypeLower === false && charTypeUpper === false && charTypeNumber === false) {
+            charSet = [specialChar,];
+        }
         // created the return value to hold the for loop value.
         // created the for loop to  run through  the differant random scenaros of the users choice.
 
-        var returnValue = [];
-
+        var returnValue = [charSet];
+        //for (var i = 0; i < charSet.length; i++) {
+        //returnValue *= charSet[Math.floor(Math.random() * charSet.length)]
+        //;
+        //}
 
         for (var i = 0; i < charLength.length; i++) {
             returnValue *= charSet[Math.floor(Math.random() * charSet.length)]
                 ;
         }
         return returnValue; // the return key will start the exacution of the function
-        //alert(generatePassword());
+
     }
-    //for (var i = 0; i < charType; i++) {
-    //returnValue += charSet(Math.floor(Math.random() * charType.length))
-    //;
-    //}
-    //return returnValue;
+
     alert(generatePassword()); // the return generatePassword will show the final result of the random password generator.
 });
 
