d661da723a59b3cae4cc79f17166583d160b827d
Brian Levin <bml201095@gmail.com>
2020-05-12 02:29:33 -0400

    created a function to generate password with the  users input

--- script.js
+++ script.js
@@ -76,21 +76,25 @@ function getRandom(arr) {
 
     return randElement;
 }
+// Function to generate password with user input
+
+function generatePassword() {
+    var options = getPasswordOptions();
 
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+    // Get references to the #generate element
+    var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector('#password');
+    // Write password to the #password input
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector('#password');
 
-    passwordText.value = password;
-}
+        passwordText.value = password;
+    }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+    // Add event listener to generate button
+    generateBtn.addEventListener('click', writePassword);
 
 
