e1b92e1dc24c2d3001572565f222c4e7031e8e55
Brian Levin <bml201095@gmail.com>
2020-05-12 01:46:02 -0400

    changed my variable array names and included the numbers in quotes which will show the genrated random numbers displayed on the page and added more special characters to my special characters array

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 // created arrays to hold values
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];  //MAth.random() b
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = ['!', '@', "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",];
+var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];  //MAth.random() b
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 
 // Added a variety of of character choice prompts for  the user to choose from.
